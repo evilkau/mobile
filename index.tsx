8d5c627d7775cc1ac9340e38385a9109f5ab4f01
evilkau <146204994+evilkau@users.noreply.github.com>
2025-04-09 12:05:48 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,35 +1,84 @@
 import { Text, View, StyleSheet } from "react-native";
-import {Image} from "expo-image"
-const foto= require ("../assets/images/react-logo.png")
+import { Image } from "expo-image"
+import { Ionicons } from "@expo/vector-icons"
+
+// npm install expo-image (usar este código no terminal)
+
+const foto = require("../assets/images/KAU.jpg")
 
 export default function Index() {
   return (
-    <View style = {estilos.container} >
-    <Image source ={foto} style={estilos.foto}/>
-    <Text style= {estilos.text}>kau</Text>
+    <View style={styles.container} >
+      <View style = {styles.containerImg}>
+        <Image source = {foto} style={styles.estiloFoto}></Image>
+      </View>
+      <View style = {styles.containerConteudo}>
+        <View style = {styles.containerNome}>
+          <Text style={styles.nome}>Kauany Vitória Taborda</Text>
+        </View>
+        <Text style = {styles.linha}>
+          _______________________________________
+        </Text>
+        <View style = {styles.containerDados}>
+          <Ionicons name="person" size={24} color="white" />
+          <Text style = {styles.textoDados}>16 anos</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="mail" size={24} color="white" />
+          <Text style = {styles.textoDados}>kauany.taborda@escola.pr.gov.br</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="call" size={24} color="white" />
+          <Text style = {styles.textoDados}>(42) 99999-6666</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="home" size={24} color="white" />
+          <Text style = {styles.textoDados}>Ponta Grossa / PR</Text>
+        </View>
+      </View>
     </View>
-      
-     ); }
-    
+  );
+}
 
-const estilos = StyleSheet.create({
-  container : {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    justifyContent :"center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  containerImg: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  estiloFoto: {
+    width: 300,
+    height: 300,
+  },
+  containerConteudo: {
+    flex: 1
+  },
+  containerNome:{
     alignItems:"center",
-    backgroundColor:"purple",
-
-
   },
-  foto:{
-  width:300,
-  height:300,
-  
+  nome: {
+    fontSize: 40,
+    color: "white",
+    fontWeight: "bold",
   },
-  text:{
-    color:"blue"
-    fontsize:20
+  linha:{
+    color: "white",
+    fontSize: 20,
+    marginBottom: 20
   },
+  containerDados: {
+    marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textoDados: {
+    marginLeft: 10,
+    color: "white",
+    fontSize: 24,
+  }
 })
-
-
